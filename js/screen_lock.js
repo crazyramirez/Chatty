cb7827ba56dda9393f9aed3f48b209716bfc5d91
crazyramirez <drleria@gmail.com>
2023-11-15 21:05:17 +0100

    wip

---
+++ js/screen_lock.js
@@ -0,0 +1,17 @@
+// Función para solicitar el modo de pantalla completa
+function activarPantallaCompleta() {
+    const elemento = document.documentElement; // Elemento raíz del documento (generalmente el <html>)
+    
+    if (elemento.requestFullscreen) {
+      elemento.requestFullscreen();
+    } else if (elemento.mozRequestFullScreen) { // Firefox
+      elemento.mozRequestFullScreen();
+    } else if (elemento.webkitRequestFullscreen) { // Chrome, Safari y Edge
+      elemento.webkitRequestFullscreen();
+    } else if (elemento.msRequestFullscreen) { // Internet Explorer/Edge
+      elemento.msRequestFullscreen();
+    }
+  }
+  
+  // Llama a la función para activar el modo de pantalla completa cuando sea necesario
+  activarPantallaCompleta();
