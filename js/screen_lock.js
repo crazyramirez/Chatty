7e4287cd3b8ad8e014aceff2bb6b0fca0bcd0b43
crazyramirez <drleria@gmail.com>
2023-11-15 21:08:31 +0100

    wip

--- js/screen_lock.js
+++ js/screen_lock.js
@@ -1,14 +1,20 @@
-// Función para solicitar el modo de pantalla completa
-function activarPantallaCompleta() {
-    const elemento = document.documentElement; // Elemento raíz del documento (generalmente el <html>)
-    
-    if (elemento.requestFullscreen) {
-      elemento.requestFullscreen();
-    } else if (elemento.mozRequestFullScreen) { // Firefox
-      elemento.mozRequestFullScreen();
-    } else if (elemento.webkitRequestFullscreen) { // Chrome, Safari y Edge
-      elemento.webkitRequestFullscreen();
-    } else if (elemento.msRequestFullscreen) { // Internet Explorer/Edge
-      elemento.msRequestFullscreen();
+// Función para solicitar un bloqueo de pantalla
+async function solicitarBloqueoPantalla() {
+    try {
+      const wakeLock = await navigator.wakeLock.request('screen');
+      console.log('Bloqueo de pantalla activado.');
+      
+      // Escuchar el evento "release" para liberar el bloqueo cuando sea necesario
+      wakeLock.addEventListener('release', () => {
+        console.log('Bloqueo de pantalla liberado.');
+      });
+  
+      // Puedes mantener el bloqueo de pantalla activo mientras sea necesario
+      // await new Promise(resolve => setTimeout(resolve, 60000)); // Ejemplo: mantener activo durante 60 segundos
+    } catch (error) {
+      console.error('Error al solicitar el bloqueo de pantalla:', error);
     }
-}
+  }
+  
+  // Llama a la función para solicitar el bloqueo de pantalla cuando sea necesario
+  solicitarBloqueoPantalla();
