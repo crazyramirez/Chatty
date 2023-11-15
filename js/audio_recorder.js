4c74a8be7e34f88a5baa27f8b5d555070979ea60
crazyramirez <drleria@gmail.com>
2023-11-15 22:51:32 +0100

    wip

--- js/audio_recorder.js
+++ js/audio_recorder.js
@@ -1,74 +1,148 @@
-//API to handle audio recording
-document.addEventListener("DOMContentLoaded", init);
+// Global Variables
+VOICE_MIN_DECIBELS      = -40;
+DELAY_BETWEEN_DIALOGS   = 1000;
+DIALOG_MAX_LENGTH       = 60*1000;
+MEDIA_RECORDER          = null;
+IS_RECORDING            = false;
 
+document.addEventListener("DOMContentLoaded", init);
 function init() {  
     navigator.mediaDevices.getUserMedia({ audio: true })
     .then(stream => {
-        const mediaRecorder = new MediaRecorder(stream);
-        mediaRecorder.start();
+        MEDIA_RECORDER = new MediaRecorder(stream);
+        MEDIA_RECORDER.start();
+        MEDIA_RECORDER.stop();
     });
 }
 
-const recordAudio = () =>
-  new Promise(async resolve => {
-    let isRecording = false;
-    let silenceTimeout;
-    const silenceThreshold = 2; // Ajusta este umbral según tus necesidades
-    
-    const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
-    const mediaRecorder = new MediaRecorder(stream);
-    const audioChunks = [];
+// Start Recording
+function startRecording() {
+    document.getElementById("mic-icon").classList.add("show");
+    IS_RECORDING = true;
+    record();
+    console.log("Start Recording");
+}
 
-    mediaRecorder.addEventListener("dataavailable", event => {
-        audioChunks.push(event.data);
-    });
+// Stop Recording
+function stopRecording(){
+    document.getElementById("mic-icon").classList.remove("show");
+    IS_RECORDING = false;
+    if(MEDIA_RECORDER !== null)
+        MEDIA_RECORDER.stop();
 
-    // Calcula el nivel de amplitud promedio del audio
-    const audioBlob = new Blob(audioChunks);
-    const audioBuffer = await audioBlob.arrayBuffer();
-    const audioData = new Float32Array(audioBuffer);
-    const amplitude =
-    audioData.reduce((acc, val) => acc + Math.abs(val), 0) / audioData.length;
+    console.log("Stop Recording");
+}
 
-    if (amplitude < silenceThreshold) {
-        // Si el nivel de amplitud es menor que el umbral, considera que hay silencio
-        if (isRecording) {
-            // Si ya se está grabando, detén la grabación
-            await stopRecording();
-        }
-    } else {
-        // Si el nivel de amplitud es mayor que el umbral, considera que hay sonido
-        if (!isRecording) {
-            // Si no se está grabando, inicia la grabación
-            mediaRecorder.start();
-            isRecording = true;
-            console.log("Start Audio Recording");
-        }
-    }
+// Record
+function record(){
 
-    // Reinicia el temporizador de silencio
-    clearTimeout(silenceTimeout);
-    silenceTimeout = setTimeout(async () => {
-        // Si se detecta silencio durante un período de tiempo, detén la grabación
-        if (isRecording) {
-            await stopRecording();
-        }
-    }, 4000); // Ajusta el tiempo de detección de silencio
-
-    const stopRecording = async () => {
-        mediaRecorder.stop();
-        isRecording = false;
-        console.log("Stop Audio Recording");
-        new Promise(resolve => {
-            mediaRecorder.addEventListener("stop", () => {
-                const audioBlob = new Blob(audioChunks);
-                const audioUrl = URL.createObjectURL(audioBlob);
-                const audio = new Audio(audioUrl);
-                audio.play();
-                console.log("Play Audio Recording");
-                resolve({ audioBlob, audioUrl });
-            });
+    navigator.mediaDevices.getUserMedia({ audio: true })
+    .then(stream => {
+        MEDIA_RECORDER = new MediaRecorder(stream);
+        // MEDIA_RECORDER.stop();
+        
+        //start recording:
+        MEDIA_RECORDER.start();
+        
+        //save audio chunks:
+        const audioChunks = [];
+        MEDIA_RECORDER.addEventListener("dataavailable", event => {
+            audioChunks.push(event.data);
         });
-        // resolve({ audioBlob, audioUrl, play });
-    };
-  });
+        
+        //analisys:
+        const audioContext      = new AudioContext();
+        const audioStreamSource = audioContext.createMediaStreamSource(stream);
+        const analyser          = audioContext.createAnalyser();
+        analyser.minDecibels    = VOICE_MIN_DECIBELS;
+        audioStreamSource.connect(analyser);
+        const bufferLength      = analyser.frequencyBinCount;
+        const domainData        = new Uint8Array(bufferLength);
+        
+        //loop:
+        let time              = new Date();
+        let startTime,
+            lastDetectedTime    = time.getTime();
+        let anySoundDetected    = false;
+        const detectSound       = () => {
+            
+            //recording stoped by user:
+            if(!IS_RECORDING)
+                return;
+
+            time = new Date();
+            currentTime = time.getTime();
+            
+            //time out:
+            if(currentTime > startTime + DIALOG_MAX_LENGTH){
+                MEDIA_RECORDER.stop();
+                return;
+            }
+
+            //a dialog detected:
+            if( anySoundDetected === true &&
+                currentTime > lastDetectedTime + DELAY_BETWEEN_DIALOGS
+                ){
+                MEDIA_RECORDER.stop();
+                return;
+            }
+            
+            //check for detection:
+            analyser.getByteFrequencyData(domainData);
+            for(let i = 0; i < bufferLength; i++)
+                if(domainData[i] > 0){
+                    anySoundDetected = true;
+                    time = new Date();
+                    lastDetectedTime = time.getTime();
+                }
+            
+            //continue the loop:
+            window.requestAnimationFrame(detectSound);
+        };
+        window.requestAnimationFrame(detectSound);
+
+        //stop event:
+        MEDIA_RECORDER.addEventListener('stop', () => {
+            console.log("Stop Audio Recording");
+            //stop all the tracks:
+            document.getElementById("mic-icon").classList.remove("show");
+            stream.getTracks().forEach(track => track.stop());
+            // stream = null;
+            IS_RECORDING = false;
+
+            const audioBlob = new Blob(audioChunks, {'type': 'audio/mpeg'});
+            uploadAudioToServer(audioBlob);
+        });
+
+    });
+}
+
+// Upload Recorded Audio to Server
+function uploadAudioToServer(audioBlob) {
+    console.log("Send Recording to Server");
+
+    // Create a Blob from audioBlob if it's not already in the correct format
+    const blob = audioBlob instanceof Blob ? audioBlob : new Blob([audioBlob], { type: 'audio/mpeg' });
+    const formData = new FormData();
+    formData.append('audio', blob, 'recording.wav');
+    formData.append("clientId", localStorage.getItem("clientId"));
+
+    fetch('/upload-audio', {
+        method: 'POST',
+        body: formData
+    })
+        .then(response => {
+            if (response.ok) {
+                return response.json(); // Parsear la respuesta como JSON
+            } else {
+                console.error('Error al enviar el audio al servidor.');
+                throw new Error('Error al enviar el audio al servidor.');
+            }
+        })
+        .then(data => {
+            console.log('Audio enviado exitosamente al servidor.');
+        })
+        .catch(error => {
+            console.error('Error en la solicitud fetch:', error);
+        });
+}
